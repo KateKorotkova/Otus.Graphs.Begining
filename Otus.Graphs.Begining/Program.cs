4328ac4eb0e802e96560f0a26c6a1e91be36da82
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2022-07-08 21:22:59 +0300

    initial commit

---
+++ Otus.Graphs.Begining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Otus.Graphs.Begining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
